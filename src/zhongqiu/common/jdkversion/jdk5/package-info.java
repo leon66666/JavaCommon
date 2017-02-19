da16e674f286c61a77890c87011d516c68dfe7ec
leon66666 <1902518544@qq.com>
2017-02-19 21:24:26 +0800

    lamdba

--- src/zhongqiu/common/jdkversion/jdk5/package-info.java
+++ src/zhongqiu/common/jdkversion/jdk5/package-info.java
@@ -7,7 +7,7 @@
  * （3）静态导入:import static java.lang.System.out;静态导入的是静态方法
  * （4）可变参数（Varargs）: int ... args
  * （5）内省（Introspector）
- * （6）泛型：泛型类；泛型作为参数和可变参数；泛型作为返回值；泛型方法；类型通配符和extend和super用法；stream转换成其他数据格式；
+ * （6）泛型：泛型类；泛型作为参数和可变参数；泛型作为返回值；泛型方法；类型通配符和extend和super用法；
  * （7）增强型for循环For-Each
  * （8）Scanner类。用来获取用户的输入
  */
