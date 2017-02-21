4d03f3ee7f5d939e03687a0c6d5e32f8317cc741
leon66666 <1902518544@qq.com>
2017-02-21 19:34:30 +0800

    add nio

--- src/zhongqiu/common/base/nio/WriteSomeBytes.java
+++ src/zhongqiu/common/base/nio/WriteSomeBytes.java
@@ -4,26 +4,26 @@ import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
 
-public class WriteSomeBytes
-{
-  static private final byte message[] = { 83, 111, 109, 101, 32,
-                                          98, 121, 116, 101, 115, 46 };
+public class WriteSomeBytes {
+	static private final byte message[] = { 83, 111, 109, 101, 32, 98, 121, 116, 101, 115, 46 };
 
-  static public void main( String args[] ) throws Exception {
-    FileOutputStream fout = new FileOutputStream( "writesomebytes.txt" );
+	static public void main(String args[]) throws Exception {
+		FileOutputStream fout = new FileOutputStream("writesomebytes.txt");
 
-    FileChannel fc = fout.getChannel();
+		// 新建通道
+		FileChannel fc = fout.getChannel();
 
-    ByteBuffer buffer = ByteBuffer.allocate( 1024 );
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-    for (int i=0; i<message.length; ++i) {
-      buffer.put( message[i] );
-    }
+		for (int i = 0; i < message.length; ++i) {
+			// 写入缓冲区
+			buffer.put(message[i]);
+		}
 
-    buffer.flip();
+		buffer.flip();
+		// 从缓冲区写入通道
+		fc.write(buffer);
 
-    fc.write( buffer );
-
-    fout.close();
-  }
+		fout.close();
+	}
 }
--- src/zhongqiu/common/base/nio/package-info.java
+++ src/zhongqiu/common/base/nio/package-info.java
@@ -2,7 +2,22 @@
  * 
  */
 /**
- * @author ngw9
+ * @author zhongqiu
  *https://www.ibm.com/developerworks/cn/education/java/j-nio/j-nio.html
+ *一、三个状态变量：position，limit，capacity。position<=limit,limit<=capacity
+ *   buffer.flip(),改变position和limit的值。
+ *   
+ *二、访问方法：get()和put()
+ *   get() 和 put() 的相对方法和绝对方法，相对方法在调用后会改变position的值，绝对方法不会改变
+ *   
+ *三、缓存区的使用。FileChannel.read() 和FileChannel. write() 调用得到了极大的简化，
+ *   因为许多工作细节都由缓冲区完成了。 clear() 和 flip() 方法用于让缓冲区在读和写之间切换。
+ *   
+ *四、缓冲区分配和包装。分配：ByteBuffer.allocate( 1024 )；
+ *               包装：byte array[] = new byte[1024];
+                     ByteBuffer buffer = ByteBuffer.wrap( array );
+  
+ *五、
+                  
  */
 package zhongqiu.common.base.nio;
