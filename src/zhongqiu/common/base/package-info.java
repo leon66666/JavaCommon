6436d89b381d03ac9b2b7125eaeedc2efbfad44a
leon66666 <1902518544@qq.com>
2017-03-02 00:49:08 +0800

    a

--- src/zhongqiu/common/base/package-info.java
+++ src/zhongqiu/common/base/package-info.java
@@ -27,8 +27,8 @@
  * （2）集合中的Stack：push，pop，peek，search。继承自vector
  * （3）集合中的Array：数组的创建，数组作为参数，作为返回值
  * （4）集合中的Arrays类，对数组的各种操作。binarySearch，sort，fill，equals，asList，tostring，hashcode，copyof
- * （5）集合中的Set：hashset,treeset,LinkedHashSet
- * （6）集合中的Map：hashmap,treemap,遍历。数组+链表；hash算法 ；初始容量(桶)和加载因子；rehash操作(扩充容量2倍)；链地址法解决冲突
+ * （5）集合中的Set：hashset,LinkedHashSet，treeset，二叉树排序，自定义排序
+ * （6）集合中的Map：hashmap,treemap,遍历。数组+链表；hash算法 ；初始容量(桶)和加载因子；rehash操作(扩充容量2倍)；链地址法解决冲突；红黑树，put和delete
  * （7）集合中的Collections类：对集合的各种操作。三种遍历方式，升序排序，降序排序，混排，自定义排序
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
  * （1）多线程 
