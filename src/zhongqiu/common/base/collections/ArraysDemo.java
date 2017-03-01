f6cf0357b98686c37ace575a41302b99fae1c77c
leon66666 <1902518544@qq.com>
2017-03-01 18:58:15 +0800

    a

--- src/zhongqiu/common/base/collections/ArraysDemo.java
+++ src/zhongqiu/common/base/collections/ArraysDemo.java
@@ -1,6 +1,7 @@
 package zhongqiu.common.base.collections;
 
 import java.util.Arrays;
+import java.util.List;
 
 //Arrays类相关
 public class ArraysDemo {
@@ -11,7 +12,7 @@ public class ArraysDemo {
 
 		int[] test3 = { 4, 5, 6, 7 };
 
-		// 根据第二个参数作为索引找出对应的下标
+		// 根据第二个参数作为索引找出对应的下标，二分法查找算法
 		System.out.println(Arrays.binarySearch(test3, 7));
 
 		// toString方法
@@ -21,7 +22,8 @@ public class ArraysDemo {
 		// Equals方法
 		System.out.println(test1.equals(test2));
 		System.out.println(Arrays.equals(test1, test2));// 重写了Equals方法
-
+		System.out.println(test1 == test2);
+		System.out.println(test1.getClass());
 		// fill方法
 		// 将指定的 byte 值分配给指定byte型数组指定范围中的每个元素。
 		Arrays.fill(test1, 0, 1, (byte) 56);
@@ -30,6 +32,10 @@ public class ArraysDemo {
 		// hashCode方法
 		System.out.println(test1.hashCode());
 		System.out.println(Arrays.hashCode(test1));// 基于指定数组的内容返回哈希码
+
+		// aslist
+		List list = Arrays.asList(test1);
+		
 	}
 
 }
--- src/zhongqiu/common/base/package-info.java
+++ src/zhongqiu/common/base/package-info.java
@@ -7,7 +7,7 @@
  * 实例：  http://www.runoob.com/java/java-examples.html
  * java基础：
  * （1）基本数据类型：8个基本类型；值类型和引用类型，常量池，intern()方法，stringbuilder
- * （2）运算符操作：a++，++a,三元运算符，instanceof运算符
+ * （2）运算符操作：a++，++a,三元运算符，instanceof运算符，位运算符
  * （3）异常：错误和异常，异常分为运行时异常和非运行时异常。Throwable，Error，Exception，RuntimeException
  * （4）String,StringBuffer,StringBuilder相关:stringbuilder的方法使用
  * （5）Number类型相关：对应基本类型的封装类。是否实现了常量池技术，哪些实现了，哪些没有实现，有哪些限制
@@ -21,13 +21,14 @@
  *                          4、Program Counter(程序计数器)5、Native method stack(本地方法栈)：保存native方法进入区域的地址
  *     JVM GC原理：对象分为年青代(Young)、年老代(Tenured)、持久代(Perm)，对不同生命周期的对象使用不同的算法
  *     年轻代和年老代都是指的JVM的Heap空间，而持久代则是之前提到的Method Area，不属于Heap。
+ * （10）object类相关方法。equals四个特性：自反，对称，一致，传递；hashcode：三个协定，需要重写的情景；getclass，toString；notify；wait
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
- * （1）集合中的List：arrayList，linkedList,vector的区别和使用场景
- * （2）集合中的Stack：push，pop，peek，search
+ * （1）集合中的List：arrayList，linkedList,vector的区别和使用场景。linkedlist双向循环链表，vector同步实现原理syn锁
+ * （2）集合中的Stack：push，pop，peek，search。继承自vector
  * （3）集合中的Array：数组的创建，数组作为参数，作为返回值
- * （4）集合中的Arrays类
+ * （4）集合中的Arrays类，对数组的各种操作。binarySearch，sort，fill，equals，asList，tostring，hashcode，copyof
  * （5）集合中的Set：hashset,treeset,LinkedHashSet
- * （6）集合中的Map：hashmap,treemap,遍历
+ * （6）集合中的Map：hashmap,treemap,遍历。数组+链表；hash算法 ；初始容量(桶)和加载因子；rehash操作(扩充容量2倍)；链地址法解决冲突
  * （7）集合中的Collections类：对集合的各种操作。三种遍历方式，升序排序，降序排序，混排，自定义排序
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
  * （1）多线程 
