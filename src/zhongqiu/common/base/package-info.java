069dd88758bdff493c2d3276153fe0713a03605f
leon66666 <1902518544@qq.com>
2017-03-14 22:54:21 +0800

    a

--- src/zhongqiu/common/base/package-info.java
+++ src/zhongqiu/common/base/package-info.java
@@ -10,7 +10,7 @@
  * （2）运算符操作：a++，++a,三元运算符，instanceof运算符，位运算符
  * （3）异常：错误和异常，异常分为运行时异常和非运行时异常。Throwable，Error，Exception，RuntimeException
  * （4）String,StringBuffer,StringBuilder相关:stringbuilder的方法使用
- * （5）Number类型相关：对应基本类型的封装类。是否实现了常量池技术，哪些实现了，哪些没有实现，有哪些限制
+ * （5）Number类型相关：对应基本类型的封装类。是否实现了常量池技术，哪些实现了，哪些没有实现，有哪些限制.存在于.class文件中，在运行期被JVM装载，并且可以扩充
  * （6）随机数。Math.random():生成随机的六位字符串
  * （7）反射机制 
  * （7）关键字：final，volatile，native，instance of，static，transient  
@@ -21,6 +21,8 @@
  *                          4、Program Counter(程序计数器)5、Native method stack(本地方法栈)：保存native方法进入区域的地址
  *     JVM GC原理：对象分为年青代(Young)、年老代(Tenured)、持久代(Perm)，对不同生命周期的对象使用不同的算法
  *     年轻代和年老代都是指的JVM的Heap空间，而持久代则是之前提到的Method Area，不属于Heap。
+ *     内存泄露：手动将生成的无用对象，中间对象置为null；对象池；弱引用；JVM调优 
+ *     内存检测工具：jconsole；JProfiler
  * （10）object类相关方法。equals四个特性：自反，对称，一致，传递；hashcode：三个协定，需要重写的情景；getclass，toString；notify；wait
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
  * （1）集合中的List：arrayList，linkedList,vector的区别和使用场景。linkedlist双向循环链表，vector同步实现原理syn锁
@@ -33,10 +35,8 @@
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
  * （1）多线程 
  * （2）Nio
- *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
  * （1）Date相关
  * （2）正则相关
- * （3）流(Stream)、文件(File)和IO
  * （4）序列化 Serialize
  * （5）网络编程 socket,tcp/ip,http
  * （6）发邮件 send mail
