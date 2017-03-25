cbbd299b89acdb772f187f8edc456b6e1769769f
leon66666 <1902518544@qq.com>
2017-03-25 19:04:10 +0800

    a

---
+++ src/zhongqiu/common/jdk7/concurrent/ForkJoinTaskDemo.java
@@ -0,0 +1,89 @@
+package zhongqiu.common.jdk7.concurrent;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinTaskDemo {
+	public static void main(String[] args) throws InterruptedException {
+		long startTime = System.currentTimeMillis();
+		int count = 0;
+		for (int i = 1; i < 10; i++) {
+			count = count + i;
+			Thread.sleep(1000);
+		}
+		System.out.println(count);
+		long endTime = System.currentTimeMillis(); // 获取结束时间
+		System.out.println("程序运行时间： " + (startTime - endTime) + "ms");
+
+		long startTime1 = System.currentTimeMillis();
+		CountTask countTask = new CountTask(1, 10);
+		ForkJoinPool forkJoinPool = new ForkJoinPool();
+		Future<Integer> futureTask = forkJoinPool.submit(countTask);
+
+		try {
+			System.out.println(futureTask.get());
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long endTime1 = System.currentTimeMillis(); // 获取结束时间
+		System.out.println("程序运行时间： " + (startTime1 - endTime1) + "ms");
+
+	}
+}
+
+// RecursiveTask用法，统计1+2+3+....
+class CountTask extends RecursiveTask<Integer> {
+	private static final int THRESHOLD = 2;
+	private Integer start;
+	private Integer end;
+
+	public CountTask(Integer start, Integer end) {
+		this.setStart(start);
+		this.setEnd(end);
+	}
+
+	@Override
+	protected Integer compute() {
+		int count = 0;
+		if (start - end <= THRESHOLD) {
+			for (int i = start; i < end; i++) {
+				count += i;
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} else {
+			int middle = (end + start) / 2;
+			CountTask leftTask = new CountTask(start, middle);
+			CountTask rightTask = new CountTask(middle + 1, end);
+			leftTask.fork();
+			rightTask.fork();
+			count = leftTask.join() + rightTask.join();
+		}
+		return count;
+	}
+
+	public Integer getStart() {
+		return start;
+	}
+
+	public void setStart(Integer start) {
+		this.start = start;
+	}
+
+	public Integer getEnd() {
+		return end;
+	}
+
+	public void setEnd(Integer end) {
+		this.end = end;
+	}
+}
--- src/zhongqiu/common/jdk7/package-info.java
+++ src/zhongqiu/common/jdk7/package-info.java
@@ -10,6 +10,7 @@
  * （3）数值可加下划线
  * （4）支持二进制文字
  * （5）【传输队列】transferqueue（tryTransfer，transfer，tryTransfer(E e, long timeout, TimeUnit unit)）
+ * （6）ForkJoinTask
  * （5）在try catch异常扑捉中，一个catch可以写多个异常类型，用"|"隔开
  * （6）自动资源管理。
  *     jdk7之前，你必须用try{}finally{}在try内使用资源，在finally中关闭资源，不管try中的代码是否正常退出或者异常退出。
