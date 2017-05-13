3c003507598c233774b4229d0bcc408bc59fc789
wangzhongqiu <git.hoomxb.com>
2017-05-13 11:43:57 +0800

    枚举相关
    1、通过默认序号ordinal获取枚举
    
    2、通过name获取枚举

--- src/zhongqiu/common/jdk5/EnumDemo.java
+++ src/zhongqiu/common/jdk5/EnumDemo.java
@@ -7,110 +7,142 @@ package zhongqiu.common.jdk5;
 4、枚举集合的使用。java.util.EnumSet和java.util.EnumMap是两个枚举集合。
   EnumSet保证集合中的元素不重复；EnumMap中的key是enum类型，而value则可以是任意类型。*/
 public class EnumDemo {
-	public static void main(String[] args) {
-		EnumSwitch();
-	}
+    public static void main(String[] args) {
+        EnumSwitch();
+    }
 
-	public enum Color {
-		RED, GREEN, BLANK, YELLOW
-	}
+    public enum Color {
+        RED, GREEN, BLANK, YELLOW;
 
-	// 自定义枚举
-	public enum ColorExt {
-		RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLOW("黄色", 4);
-		// 成员变量
-		private String name;
-		private int index;
+        //通过name获取枚举
+        public static Color getByName(String name) {
+            Color color = Color.RED;
+            switch (name) {
+                case "RED":
+                    color = Color.RED;
+                    break;
+                case "GREEN":
+                    color = Color.GREEN;
+                    break;
+                case "BLANK":
+                    color = Color.BLANK;
+                    break;
+                case "YELLOW":
+                    color = Color.YELLOW;
+                    break;
+                default:
+                    color = null;
+                    break;
+            }
+            return color;
+        }
 
-		// 构造方法
-		private ColorExt(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
+        //通过默认序号ordinal获取枚举
+        public static Color valueOf(int ordinal) {
+            if (ordinal >= 0 && ordinal < values().length) {
+                return values()[ordinal];
+            } else {
+                throw new IndexOutOfBoundsException("Invalid ordinal");
+            }
+        }
+    }
 
-		// 普通方法
-		public static String getName(int index) {
-			for (ColorExt c : ColorExt.values()) {
-				if (c.getIndex() == index) {
-					return c.name;
-				}
-			}
-			return null;
-		}
+    // 自定义枚举
+    public enum ColorExt {
+        RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLOW("黄色", 4);
+        // 成员变量
+        private String name;
+        private int index;
 
-		// 覆盖方法
-		@Override
-		public String toString() {
-			return this.index + "_" + this.name;
-		}
+        // 构造方法
+        private ColorExt(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
 
-		// get set 方法
-		public String getName() {
-			return name;
-		}
+        // 普通方法
+        public static String getName(int index) {
+            for (ColorExt c : ColorExt.values()) {
+                if (c.getIndex() == index) {
+                    return c.name;
+                }
+            }
+            return null;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        // 覆盖方法
+        @Override
+        public String toString() {
+            return this.index + "_" + this.name;
+        }
 
-		public int getIndex() {
-			return index;
-		}
+        // get set 方法
+        public String getName() {
+            return name;
+        }
 
-		public void setIndex(int index) {
-			this.index = index;
-		}
-	}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-	// 枚举的switch用法
-	public static void EnumSwitch() {
-		Color color = Color.RED;
-		System.out.println(color);
-		switch (color) {
-		case RED:
-			color = Color.GREEN;
-			System.out.println(color);
-			break;
-		case YELLOW:
-			color = Color.RED;
-			System.out.println(color);
-			break;
-		case GREEN:
-			color = Color.YELLOW;
-			System.out.println(color);
-			break;
-		}
-	}
+        public int getIndex() {
+            return index;
+        }
 
-	// 枚举实现接口
-	public interface Behaviour {
-		void print();
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
 
-		String getInfo();
-	}
+    // 枚举的switch用法
+    public static void EnumSwitch() {
+        Color color = Color.RED;
+        System.out.println(color);
+        switch (color) {
+            case RED:
+                color = Color.GREEN;
+                System.out.println(color);
+                break;
+            case YELLOW:
+                color = Color.RED;
+                System.out.println(color);
+                break;
+            case GREEN:
+                color = Color.YELLOW;
+                System.out.println(color);
+                break;
+        }
+    }
 
-	public enum ColorImpl implements Behaviour {
-		RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLOW("黄色", 4);
-		// 成员变量
-		private String name;
-		private int index;
+    // 枚举实现接口
+    public interface Behaviour {
+        void print();
 
-		// 构造方法
-		private ColorImpl(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
+        String getInfo();
+    }
 
-		// 接口方法
-		@Override
-		public String getInfo() {
-			return this.name;
-		}
+    public enum ColorImpl implements Behaviour {
+        RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLOW("黄色", 4);
+        // 成员变量
+        private String name;
+        private int index;
 
-		// 接口方法
-		@Override
-		public void print() {
-			System.out.println(this.index + ":" + this.name);
-		}
-	}
+        // 构造方法
+        private ColorImpl(String name, int index) {
+            this.name = name;
+            this.index = index;
+        }
+
+        // 接口方法
+        @Override
+        public String getInfo() {
+            return this.name;
+        }
+
+        // 接口方法
+        @Override
+        public void print() {
+            System.out.println(this.index + ":" + this.name);
+        }
+    }
 }
