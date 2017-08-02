f6a1016a91480c16df4855571c03b6ef542b6093
leon66666 <1902518544@qq.com>
2017-08-02 19:06:37 +0800

    快排

--- src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
+++ src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
@@ -4,5 +4,45 @@ package zhongqiu.common.base.algorithm.sort;
  * Created by wangzhongqiu on 2017/8/2.
  */
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 4, 3, 2, 1};
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(arr, 0, arr.length-1);
+    }
 
+    public void sort(int[] arr, int low, int high) {
+        int l = low;
+        int h = high;
+        int povit = arr[low];
+        while (l < h) {
+            while (l < h && arr[h] >= povit)
+                h--;
+            if (l < h) {
+                int temp = arr[h];
+                arr[h] = arr[l];
+                arr[l] = temp;
+                l++;
+            }
+
+            while (l < h && arr[l] <= povit)
+                l++;
+
+            if (l < h) {
+                int temp = arr[h];
+                arr[h] = arr[l];
+                arr[l] = temp;
+                h--;
+            }
+        }
+        print(arr);
+        System.out.print("l=" + (l + 1) + ",h=" + (h + 1) + ",povit=" + povit + "\n");
+        if (l > low) sort(arr, low, l - 1);
+        if (h < high) sort(arr, l + 1, high);
+    }
+
+    public void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + ",");
+        }
+    }
 }
