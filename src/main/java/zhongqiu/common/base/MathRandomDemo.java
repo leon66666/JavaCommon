0bb4506adbbc1c097e9b5d8b4f4395bb622beb79
leon66666 <1902518544@qq.com>
2017-08-04 18:33:57 +0800

    算法性能测试

--- src/main/java/zhongqiu/common/base/MathRandomDemo.java
+++ src/main/java/zhongqiu/common/base/MathRandomDemo.java
@@ -1,35 +1,88 @@
 package zhongqiu.common.base;
 
+import java.util.Random;
+
 //随机数。Math.random() -- 返回0和1之间的伪随机数 可能为0，但总是小于1，[0,1)
 public class MathRandomDemo {
-	public static void main(String[] args) {
-		intAndChar();
-		for (int i = 0; i < 100; i++) {
-			System.out.println(Math.random());
-		}
-		for (int i = 0; i < 10; i++) {
-			sixRandomStr();
-		}
+    public static void main(String[] args) {
+        intAndChar();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Math.random());
+        }
+        for (int i = 0; i < 10; i++) {
+            sixRandomStr();
+        }
+    }
 
-	}
+    // int和char对应关系
+    public static void intAndChar() {
+        for (int i = 1; i < 257; i++) {
+            System.out.println(i + "对应:" + (char) i);
+        }
+    }
 
-	// int和char对应关系
-	public static void intAndChar() {
-		for (int i = 1; i < 257; i++) {
-			System.out.println(i + "对应:" + (char) i);
-		}
-	}
+    // 生成一个6位的随机字符串
+    public static void sixRandomStr() {
+        String result = "";
+        for (int i = 0; i < 6; i++) {
+            // 生成97-122的int型的整型
+            int intValue = (int) (Math.random() * 25 + 97);
+            // 将intValue强制转化成char类型后接到result后面
+            result = result + (char) intValue;
+        }
+        // 输出字符串
+        System.out.println(result);
+    }
 
-	// 生成一个6位的随机字符串
-	public static void sixRandomStr() {
-		String result = "";
-		for (int i = 0; i < 6; i++) {
-			// 生成97-122的int型的整型
-			int intValue = (int) (Math.random() * 25 + 97);
-			// 将intValue强制转化成char类型后接到result后面
-			result = result + (char) intValue;
-		}
-		// 输出字符串
-		System.out.println(result);
-	}
+    public static int getRadom(int i) {
+        Random r = new Random();
+        return r.nextInt(i);
+    }
+
+    //获取长度为n的随机数组，局限性：数组里会有重复
+    public static int[] getRandomIntArrayWithRepeat(int n) {
+        Random r = new Random();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = r.nextInt(n);
+        }
+        return a;
+    }
+
+    /**
+     * 随机指定范围内N个不重复的数
+     * 在初始化的无重复待选数组中随机产生一个数放入结果中，
+     * 将待选数组被随机到的数，用待选数组(len-1)下标对应的数替换
+     * 然后从len-2里随机产生下一个随机数，如此类推
+     * @param max  指定范围最大值
+     * @param min  指定范围最小值
+     * @param n  随机数个数
+     * @return int[] 随机数结果集
+     */
+    public static int[] getRandomIntArrayWithoutRepeat(int min,int max,int n){
+        int len = max-min+1;
+
+        if(max < min || n > len){
+            return null;
+        }
+
+        //初始化给定范围的待选数组
+        int[] source = new int[len];
+        for (int i = min; i < min+len; i++){
+            source[i-min] = i;
+        }
+
+        int[] result = new int[n];
+        Random rd = new Random();
+        int index = 0;
+        for (int i = 0; i < result.length; i++) {
+            //待选数组0到(len-2)随机一个下标
+            index = Math.abs(rd.nextInt() % len--);
+            //将随机到的数放入结果集
+            result[i] = source[index];
+            //将待选数组中被随机到的数，用待选数组(len-1)下标对应的数替换
+            source[index] = source[len];
+        }
+        return result;
+    }
 }
--- src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
+++ src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
@@ -1,17 +1,100 @@
 package zhongqiu.common.base.algorithm.sort;
 
+import zhongqiu.common.base.MathRandomDemo;
+
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by wangzhongqiu on 2017/8/2.
  * 快速排序
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {7, 5, 4, 3, 2, 1};
+        //生成准备数据
         QuickSort quickSort = new QuickSort();
-//        quickSort.sort_one(arr, 0, arr.length - 1);
-        quickSort.sort_two(arr, 0, arr.length - 1);
-        quickSort.print(arr);
+        int n=100000;
+//        int[] arr = MathRandomDemo.getRandomIntArrayWithRepeat(n);
+        int[] arr = MathRandomDemo.getRandomIntArrayWithoutRepeat(0,n-1, n);
+        int[] brr = arr;
+//        quickSort.print(arr);
+
+        //开始计时，排序开始
+        Date arrBegin = new Date();
+        quickSort.sort_one(arr, 0, arr.length - 1);
+        Date arrEnd = new Date();
+        System.out.println("sort_one总运行时间:" + (arrEnd.getTime() - arrBegin.getTime()) + "毫秒");
+        System.out.println("sort_one总运行次数:" + quickSort.getNum());
+//        quickSort.print(arr);
+
+        //开始计时，排序开始
+        Date brrBegin = new Date();
+        quickSort.sort_two(brr, 0, arr.length - 1);
+        Date brrEnd = new Date();
+        System.out.println("sort_two总运行时间:" + (brrEnd.getTime() - brrBegin.getTime()) + "毫秒");
+        System.out.println("sort_two总运行次数:" + quickSort.getNum());
+//        quickSort.print(brr);
     }
+
+    //经典写法
+    public void sort_one(int[] arr, int low, int high) {
+        num++;
+        if (low < high) {
+            int l = low;
+            int h = high;
+            int pivot = arr[low];
+            while (l < h) {
+                while (l < h && arr[h] >= pivot)
+                    h--;
+                if (l < h) {
+                    int temp = arr[h];
+                    arr[h] = arr[l];
+                    arr[l] = temp;
+                    l++;
+                }
+
+                while (l < h && arr[l] <= pivot)
+                    l++;
+
+                if (l < h) {
+                    int temp = arr[h];
+                    arr[h] = arr[l];
+                    arr[l] = temp;
+                    h--;
+                }
+            }
+//            print(arr);
+//            System.out.print("l=" + l + ",low=" + low + ",h=" + h + ",high=" + high + ",pivot=" + pivot + "\n");
+            sort_one(arr, low, l - 1);
+            sort_one(arr, l + 1, high);
+        }
+    }
+
+    //优化之后的快速排序算法，减少执行次数，不用每次都互换值
+    void sort_two(int[] arr, int low, int high) {
+        num++;
+        if (low < high) {
+            int l = low, h = high, pivot = arr[low];
+            while (l < h) {
+                while (l < h && arr[h] >= pivot) // 从右向左找第一个小于x的数
+                    h--;
+                if (l < h) {
+                    arr[l] = arr[h];
+                    l++;
+                }
+                while (l < h && arr[l] < pivot) // 从左向右找第一个大于等于x的数
+                    l++;
+                if (l < h) {
+                    arr[h] = arr[l];
+                    h--;
+                }
+            }
+            arr[l] = pivot;
+            if (l > low) sort_two(arr, low, l - 1); // 递归调用
+            if (h < high) sort_two(arr, l + 1, high);
+        }
+    }
+
 
     private Integer num = 0;
 
@@ -23,70 +106,10 @@ public class QuickSort {
         this.num = num;
     }
 
-    //经典写法
-    public void sort_one(int[] arr, int low, int high) {
-        num++;
-        System.out.println("执行次数=" + num);
-        if (low < high) {
-            int l = low;
-            int h = high;
-            int povit = arr[low];
-            while (l < h) {
-                while (l < h && arr[h] >= povit)
-                    h--;
-                if (l < h) {
-                    int temp = arr[h];
-                    arr[h] = arr[l];
-                    arr[l] = temp;
-                    l++;
-                }
-
-                while (l < h && arr[l] <= povit)
-                    l++;
-
-                if (l < h) {
-                    int temp = arr[h];
-                    arr[h] = arr[l];
-                    arr[l] = temp;
-                    h--;
-                }
-            }
-            print(arr);
-            System.out.print("l=" + l + ",low=" + low + ",h=" + h + ",high=" + high + ",povit=" + povit + "\n");
-            sort_one(arr, low, l - 1);
-            sort_one(arr, l + 1, high);
-        }
-    }
-
-    //优化之后的快速排序算法，减少执行次数，不用每次都互换值
-    void sort_two(int[] arr, int low, int high) {
-        num++;
-        System.out.println("执行次数=" + num);
-        if (low < high) {
-            int l = low, h = high, povit = arr[low];
-            while (l < h) {
-                while (l < h && arr[h] >= povit) // 从右向左找第一个小于x的数
-                    h--;
-                if (l < h) {
-                    arr[l] = arr[h];
-                    l++;
-                }
-                while (l < h && arr[l] < povit) // 从左向右找第一个大于等于x的数
-                    l++;
-                if (l < h) {
-                    arr[h] = arr[l];
-                    h--;
-                }
-            }
-            arr[l] = povit;
-            if (l > low) sort_two(arr, low, l - 1); // 递归调用
-            if (h < high) sort_two(arr, l + 1, high);
-        }
-    }
-
     public void print(int[] arr) {
         for (int i : arr) {
             System.out.print(i + ",");
         }
+        System.out.println("");
     }
 }
