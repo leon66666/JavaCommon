256453ef8921cdd4eed876d6465bbf42ab574ff5
leon66666 <1902518544@qq.com>
2017-08-05 14:10:19 +0800

    算法性能测试

--- src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
+++ src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
@@ -12,27 +12,30 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) {
         //生成准备数据
-        QuickSort quickSort = new QuickSort();
-        int n=100000;
-//        int[] arr = MathRandomDemo.getRandomIntArrayWithRepeat(n);
-        int[] arr = MathRandomDemo.getRandomIntArrayWithoutRepeat(0,n-1, n);
-        int[] brr = arr;
-//        quickSort.print(arr);
+        int n = 1000000;
+        int[] arr = MathRandomDemo.getRandomIntArrayWithRepeat(n);
+//        int[] arr = MathRandomDemo.getRandomIntArrayWithoutRepeat(0,n-1, n);
+        int[] brr = new int[n];
+        for (int i = 0; i < n; i++) {
+            brr[i] = arr[i];
+        }
+        QuickSort quickSort_one = new QuickSort();
+        QuickSort quickSort_two = new QuickSort();
 
         //开始计时，排序开始
         Date arrBegin = new Date();
-        quickSort.sort_one(arr, 0, arr.length - 1);
+        quickSort_one.sort_one(arr, 0, arr.length - 1);
         Date arrEnd = new Date();
         System.out.println("sort_one总运行时间:" + (arrEnd.getTime() - arrBegin.getTime()) + "毫秒");
-        System.out.println("sort_one总运行次数:" + quickSort.getNum());
+        System.out.println("sort_one总运行次数:" + quickSort_one.getNum());
 //        quickSort.print(arr);
 
         //开始计时，排序开始
         Date brrBegin = new Date();
-        quickSort.sort_two(brr, 0, arr.length - 1);
+        quickSort_two.sort_two(brr, 0, brr.length - 1);
         Date brrEnd = new Date();
         System.out.println("sort_two总运行时间:" + (brrEnd.getTime() - brrBegin.getTime()) + "毫秒");
-        System.out.println("sort_two总运行次数:" + quickSort.getNum());
+        System.out.println("sort_two总运行次数:" + quickSort_two.getNum());
 //        quickSort.print(brr);
     }
 
@@ -69,8 +72,41 @@ public class QuickSort {
             sort_one(arr, l + 1, high);
         }
     }
+    //最优写法（一下情境下最优：1、带有重复元素的数组 ）
+/*    public void sort_two(int[] arr, int low, int high) {
+        num++;
+        if (low < high) {
+            int l = low;
+            int h = high;
+            int pivot = arr[low];
+            while (l < h) {
+                while (l < h && arr[h] >= pivot)
+                    h--;
+                if (l < h) {
+                    int temp = arr[h];
+                    arr[h] = arr[l];
+                    arr[l] = temp;
+                    l++;
+                }
 
-    //优化之后的快速排序算法，减少执行次数，不用每次都互换值
+                while (l < h && arr[l] <= pivot)
+                    l++;
+
+                if (l < h) {
+                    int temp = arr[h];
+                    arr[h] = arr[l];
+                    arr[l] = temp;
+                    h--;
+                }
+            }
+//            print(arr);
+//            System.out.print("l=" + l + ",low=" + low + ",h=" + h + ",high=" + high + ",pivot=" + pivot + "\n");
+            if (l > low) sort_two(arr, low, l - 1);
+            if (h < high) sort_two(arr, l + 1, high);
+        }
+    }*/
+
+
     void sort_two(int[] arr, int low, int high) {
         num++;
         if (low < high) {
@@ -90,8 +126,8 @@ public class QuickSort {
                 }
             }
             arr[l] = pivot;
-            if (l > low) sort_two(arr, low, l - 1); // 递归调用
-            if (h < high) sort_two(arr, l + 1, high);
+            sort_two(arr, low, l - 1); // 递归调用
+            sort_two(arr, l + 1, high);
         }
     }
 
