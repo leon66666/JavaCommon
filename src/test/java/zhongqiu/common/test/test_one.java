97dd152318c1b8731098c1f3845483787af6fa16
leon66666 <1902518544@qq.com>
2017-08-07 10:04:45 +0800

    快排，优化

--- src/test/java/zhongqiu/common/test/test_one.java
+++ src/test/java/zhongqiu/common/test/test_one.java
@@ -1,7 +1,13 @@
 package zhongqiu.common.test;
 
 /**
- * Created by hxb002 on 2017/8/6.
+ * Created by wangzhongqiu on 2017/8/6.
  */
 public class test_one {
+    public static void main(String[] args) {
+        System.out.println(222);
+        for (int i = 4; i < 2; i++) {
+            System.out.print(111);
+        }
+    }
 }
