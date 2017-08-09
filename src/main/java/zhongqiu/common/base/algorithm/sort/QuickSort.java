d3afe69a69923c5b5e7bf2a0448e8ecae55acfc3
leon66666 <1902518544@qq.com>
2017-08-09 21:01:29 +0800

    快排，优化

--- src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
+++ src/main/java/zhongqiu/common/base/algorithm/sort/QuickSort.java
@@ -159,6 +159,39 @@ public class QuickSort {
         }
     }
 
+    //不需要互换的写法,不需要创建大量的临时变量。三数取中选取枢轴。长度小于10采用插入排序。优化递归操作。聚集相等元素
+    //对于很小和部分有序的数组，快排不如插排好。当待排序序列的长度分割到一定大小后，继续分割的效率比插入排序要差，此时可以使用插排而不是快排
+    void sort_five(int[] arr, int low, int high) {
+        num++;
+        if (low < high) {
+            if (high - low + 1 < 10) {
+                InsertSort.insertSort(arr, low, high);
+                return;
+            }
+            int l = low, h = high;
+            int pivot = SelectPivotMedianOfThree(arr, l, h);
+
+
+            while (l < h) {
+                while (l < h && arr[h] >= pivot) // 从右向左找第一个小于x的数
+                    h--;
+                if (l < h) {
+                    arr[l] = arr[h];
+                    l++;
+                }
+                while (l < h && arr[l] < pivot) // 从左向右找第一个大于等于x的数
+                    l++;
+                if (l < h) {
+                    arr[h] = arr[l];
+                    h--;
+                }
+            }
+            arr[l] = pivot;
+            if (l > low) sort_five(arr, low, l - 1);//优化递归操作
+            if (h < high) sort_five(arr, l + 1, high);//优化递归操作
+        }
+    }
+
     /*函数作用：取待排序序列中low、mid、high三个位置上数据，选取他们中间的那个数据作为枢轴*/
     public int SelectPivotMedianOfThree(int[] arr, int low, int high) {
         int mid = low + ((high - low) >> 1);//计算数组中间的元素的下标
