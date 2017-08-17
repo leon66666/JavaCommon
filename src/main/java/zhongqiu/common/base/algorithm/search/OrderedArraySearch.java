cd0412d7cde53fa2708e58bdb06576071fb0fccb
leon66666 <1902518544@qq.com>
2017-08-17 16:04:25 +0800

    查找算法

--- src/main/java/zhongqiu/common/base/algorithm/search/OrderedArraySearch.java
+++ src/main/java/zhongqiu/common/base/algorithm/search/OrderedArraySearch.java
@@ -1,7 +1,44 @@
 package zhongqiu.common.base.algorithm.search;
 
+import java.util.Scanner;
+
 /**
- * Created by hxb002 on 2017/8/17.
+ * Created by wangzhongqiu on 2017/8/17.
  */
 public class OrderedArraySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        int[][] array = new int[columns][rows];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+        OrderedArraySearch orderedArraySearch = new OrderedArraySearch();
+        System.out.print(orderedArraySearch.find(7, array));
+    }
+
+    public boolean find(int target, int[][] array) {
+        boolean found = false;
+        int rows = array.length;
+        int columns = array[0].length;
+        if (array != null && rows > 0 && columns > 0) {
+            int r = 0;
+            int c = columns - 1;
+            while (r < rows && c >= 0) {
+                int temp = array[r][c];
+                if (target > temp) {
+                    r++;
+                } else if (target < temp) {
+                    c--;
+                } else {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
 }
--- src/main/java/zhongqiu/common/base/algorithm/search/SequentialSearch.java
+++ src/main/java/zhongqiu/common/base/algorithm/search/SequentialSearch.java
@@ -1,7 +1,20 @@
 package zhongqiu.common.base.algorithm.search;
 
 /**
- * Created by hxb002 on 2017/8/17.
+ * Created by wangzhongqiu on 2017/8/17.
+ * 顺序查找
  */
 public class SequentialSearch {
+    public static void mian(String[] args) {
+
+    }
+
+    public boolean find(int target, int[][] array) {
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array[x].length; y++) {
+                if (array[x][y] == target) return true;
+            }
+        }
+        return false;
+    }
 }
