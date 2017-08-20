395a2a4af943b31303b6117355cadbeedac59912
leon66666 <1902518544@qq.com>
2017-08-20 22:24:26 +0800

    遍历链表

--- src/test/java/zhongqiu/common/test/test_two.java
+++ src/test/java/zhongqiu/common/test/test_two.java
@@ -9,4 +9,27 @@ public class test_two {
     public static void main(String[] args) {
 
     }
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (listNode != null) {
+            arrayList.add(listNode.val);
+            listNode = listNode.next;
+        }
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            newArrayList.add(arrayList.get(arrayList.size() - 1 - i));
+        }
+        return newArrayList;
+    }
 }
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
