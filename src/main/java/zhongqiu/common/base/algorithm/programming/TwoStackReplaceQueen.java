9f11b220c1c8dc8de3407b55c1e52f789c260384
leon66666 <1902518544@qq.com>
2017-08-22 11:09:18 +0800

    剑指offer：两个栈实现队列

---
+++ src/main/java/zhongqiu/common/base/algorithm/programming/TwoStackReplaceQueen.java
@@ -0,0 +1,41 @@
+package zhongqiu.common.base.algorithm.programming;
+
+import java.util.Stack;
+
+/**
+ * Created by wangzhongqiu on 2017/8/22.
+ * 剑指offer：两个栈实现队列的功能。实现push和pop方法
+ */
+public class TwoStackReplaceQueen {
+    public static void main(String[] args) {
+        TwoStackReplaceQueen test = new TwoStackReplaceQueen();
+        test.push(1);
+        test.push(2);
+        test.push(3);
+        System.out.println(test.pop());
+        System.out.println(test.pop());
+        System.out.println(test.pop());
+        System.out.println(test.pop());
+    }
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        if (stack1.isEmpty()) {
+            stack1.push(node);
+        } else {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            stack1.push(node);
+            while (!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+        }
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+}
--- src/test/java/zhongqiu/common/test/test_two.java
+++ src/test/java/zhongqiu/common/test/test_two.java
@@ -10,52 +10,5 @@ public class test_two {
 
     }
 
-    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        if (pre == null || in == null || pre.length <= 0 || in.length <= 0) {
-            return null;
-        }
-        return ConstructCore(pre, 0, pre.length - 1, in, 0, in.length - 1);
-    }
-
-    public TreeNode ConstructCore(int[] preOrder, int startPreIndex, int endPreIndex,
-                                  int[] inOrder, int startInIndex, int endInIndex) {
-        int rootValue = preOrder[startPreIndex];
-        TreeNode root = new TreeNode(rootValue);
-        // 只有一个元素
-        if (startPreIndex == endPreIndex) {
-            if (startInIndex == endInIndex && preOrder[startPreIndex] == inOrder[startInIndex]) {
-                return root;
-            }
-        }
-        // 在中序遍历中找到根结点的索引
-        int rootInIndex = startInIndex;
-        while (rootInIndex <= endInIndex && inOrder[rootInIndex] != rootValue) {
-            rootInIndex++;
-        }
-        int leftLength = rootInIndex - startInIndex;
-        int leftPreOrderEndIndex = startPreIndex + leftLength;
-        if (leftLength > 0) {
-            // 构建左子树
-            root.left = ConstructCore(preOrder, startPreIndex + 1,
-                    leftPreOrderEndIndex, inOrder, startInIndex,
-                    rootInIndex - 1);
-        }
-        if (leftLength < endPreIndex - startPreIndex) {
-            // 右子树有元素,构建右子树
-            root.right = ConstructCore(preOrder, leftPreOrderEndIndex + 1,
-                    endPreIndex, inOrder, rootInIndex + 1, endInIndex);
-        }
-        return root;
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
 }
 
