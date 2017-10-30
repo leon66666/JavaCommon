44f5dfbe623ca56c58352f08425d10eb172b531b
leon66666 <1902518544@qq.com>
2017-10-30 22:33:46 +0800

    test

--- src/test/java/zhongqiu/common/test/test.java
+++ src/test/java/zhongqiu/common/test/test.java
@@ -3,14 +3,8 @@ package zhongqiu.common.test;
 *
 * */
 
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 public class test {
     public static void main(String[] args) {
-        String filePathTemp = "C:/Users/Sun/Desktop/测试.xls";
-        String REGEX = "/";
-        String aaa=Pattern.compile(REGEX).matcher(filePathTemp).replaceAll("\\\\\\\\");
-        System.out.print(aaa);
+
     }
 }
