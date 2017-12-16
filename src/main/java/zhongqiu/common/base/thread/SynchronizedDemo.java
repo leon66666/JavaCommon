7d1d0b7663d1a1bd23d84d09455edae91b1f824c
leon66666 <1902518544@qq.com>
2017-12-16 19:21:15 +0800

    first version

--- src/main/java/zhongqiu/common/base/thread/SynchronizedDemo.java
+++ src/main/java/zhongqiu/common/base/thread/SynchronizedDemo.java
@@ -3,14 +3,17 @@ package zhongqiu.common.base.thread;
 //多线程资源同步
 /*
 1、synchronized关键字的作用域有二种：
-1）是某个对象实例内，synchronized aMethod(){}可以防止多个线程同时访问这个对象的synchronized方法（如果一个对象有多个synchronized方法，
-       只要一个线程访问了其中的一个synchronized方法，其它线程不能同时访问这个对象中任何一个synchronized方法）。
-       这时，不同的对象实例的synchronized方法是不相干扰的。也就是说，其它线程照样可以同时访问相同类的另一个对象实例中的synchronized方法；
+1）是某个对象实例内，synchronized aMethod(){}可以防止多个线程同时访问这个对象的synchronized方法
+      （如果一个对象有多个synchronized方法，只要一个线程访问了其中的一个synchronized方法，
+       其它线程不能同时访问这个对象中任何一个synchronized方法）。
+       这时，不同的对象实例的synchronized方法是不相干扰的。
+       也就是说，其它线程照样可以同时访问相同类的另一个对象实例中的synchronized方法；
 2）是某个类的范围，synchronized static aStaticMethod{}防止多个线程同时访问这个类中的synchronized static 方法。它可以对类的所有对象实例起作用。
-2、除了方法前用synchronized关键字，synchronized关键字还可以用于方法中的某个区块中，表示只对这个区块的资源实行互斥访问。
-       用法是: synchronized(this){区块}，它的作用域是当前对象；
-3、synchronized关键字是不能继承的，也就是说，基类的方法synchronized f(){} 在继承类中并不自动是synchronized f(){}，
-       而是变成了f(){}。继承类需要你显式的指定它的某个方法为synchronized方法；
+2、除了方法前用synchronized关键字，synchronized关键字还可以用于方法中的某个区块中，
+   表示只对这个区块的资源实行互斥访问。用法是: synchronized(this){区块}，它的作用域是当前对象；
+3、synchronized关键字是不能继承的，也就是说，
+   基类的方法synchronized f(){} 在继承类中并不自动是synchronized f(){}，
+   而是变成了f(){}。继承类需要你显式的指定它的某个方法为synchronized方法；
 */
 public class SynchronizedDemo implements Runnable {
     public static void main(String[] args) {
