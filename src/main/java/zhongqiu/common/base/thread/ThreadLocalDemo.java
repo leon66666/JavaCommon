8ea52c4ef327c87ed05ef0577b84059199fd32ed
leon66666 <1902518544@qq.com>
2018-01-18 19:03:49 +0800

    first version

--- src/main/java/zhongqiu/common/base/thread/ThreadLocalDemo.java
+++ src/main/java/zhongqiu/common/base/thread/ThreadLocalDemo.java
@@ -6,19 +6,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ThreadLocalDemo {
-	private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-	@SuppressWarnings("rawtypes")
-	private static ThreadLocal threadLocal = new ThreadLocal() {
-		protected synchronized Object initialValue() {
-			return new SimpleDateFormat(DATE_FORMAT);
-		}
-	};
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    @SuppressWarnings("rawtypes")
+    private static ThreadLocal threadLocal = new ThreadLocal() {
+        protected synchronized Object initialValue() {
+            return new SimpleDateFormat(DATE_FORMAT);
+        }
+    };
 
-	public static DateFormat getDateFormat() {
-		return (DateFormat) threadLocal.get();
-	}
+    public static DateFormat getDateFormat() {
+        return (DateFormat) threadLocal.get();
+    }
 
-	public static Date parse(String textDate) throws ParseException {
-		return getDateFormat().parse(textDate);
-	}
+    public static Date parse(String textDate) throws ParseException {
+        return getDateFormat().parse(textDate);
+    }
 }
