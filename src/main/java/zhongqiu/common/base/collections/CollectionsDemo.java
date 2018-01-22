cc6fccf4579884a28b83069c2e0187724b78bd0f
leon66666 <1902518544@qq.com>
2018-01-22 21:11:14 +0800

    first version

--- src/main/java/zhongqiu/common/base/collections/CollectionsDemo.java
+++ src/main/java/zhongqiu/common/base/collections/CollectionsDemo.java
@@ -1,8 +1,20 @@
 package zhongqiu.common.base.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author wangzhongqiu
  * @date 2018/1/22.
  */
 public class CollectionsDemo {
+    private static ArrayList<String> lists = new ArrayList<>();
+
+    public static void main(String[] args) {
+        lists.add("1");
+        lists.add("2");
+        lists.add("3");
+        List<String> synlist = Collections.synchronizedList(lists);
+    }
 }
