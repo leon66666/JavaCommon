87a92dabc37904aea95a3900ad985019ccba5b7e
leon66666 <1902518544@qq.com>
2018-01-23 17:59:05 +0800

    first version

---
+++ src/main/java/zhongqiu/common/base/thread/SimpleDateFormatDemo.java
@@ -0,0 +1,13 @@
+package zhongqiu.common.base.thread;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author wangzhongqiu
+ * @date 2018/1/23.
+ */
+public class SimpleDateFormatDemo {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+    }
+}
