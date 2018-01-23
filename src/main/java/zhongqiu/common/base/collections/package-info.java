dfc39b5018ddd29a4c92520c310ca54af980425d
leon66666 <1902518544@qq.com>
2018-01-23 16:23:15 +0800

    first version

--- src/main/java/zhongqiu/common/base/collections/package-info.java
+++ src/main/java/zhongqiu/common/base/collections/package-info.java
@@ -25,8 +25,9 @@
  * （2）集合中的Stack：push，pop，peek，search。【Stack extends Vector】
  * （3）集合中的Array：数组的创建，数组作为参数，作为返回值
  * （4）集合中的Arrays类，对数组的各种操作。binarySearch，sort，fill，equals，asList，tostring，hashcode，copyof
- * （5）集合中的Set：hashset,LinkedHashSet，treeset，二叉树排序，自定义排序
- * （6）集合中的Map：hashmap,treemap
+ *  (5) Collections.synchronizedList。装饰器模式。遍历器遍历的时候，需要手动加锁
+ * （6）集合中的Set：hashset,LinkedHashSet，treeset，二叉树排序，自定义排序
+ * （7）集合中的Map：hashmap,treemap
        【应用】统计字符串中每个字符出现的次数。统计字符串中的大写，小写，数字，其他字符个数。统计字符串中子字符串出现的次数
        【内部实现】数组+链表；hash算法 ；初始容量(桶)和加载因子；rehash操作(扩充容量2倍)；链地址法解决冲突；红黑树，put和delete
        【HashMap】【HashMap implements Map】
