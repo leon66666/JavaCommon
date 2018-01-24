87b3a25d509c7a8f750bf7acc14d13d2dffe80b0
leon66666 <1902518544@qq.com>
2018-01-24 18:46:48 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/ConcurrentHashMapDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/ConcurrentHashMapDemo.java
@@ -1,7 +1,14 @@
 package zhongqiu.common.jdk5.concurrent;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 //http://www.cnblogs.com/wangzhongqiu/p/6464230.html
 //源码解析：http://www.importnew.com/22007.html
 public class ConcurrentHashMapDemo {
+    private static ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap();
 
+    public static void main(String[] args) {
+        concurrentHashMap.put("1", "111");
+        concurrentHashMap.get("1");
+    }
 }
--- src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
@@ -1,8 +1,11 @@
 /*
  * @author zhongqiu
  * jdk5特性
- * （1）
- * （2）
+ * （1）ConcurrentHashMap.持有对象：Segment<K,V>[] segments;segmentMask;segmentShift
+ *        【Segment extends ReentrantLock】HashEntry<K,V>[] table;modCount;threshold;count;loadFactor
+ *        【HashEntry】final hash；final key；value；HashEntry<K,V> next;
+ * （2）ReentrantLock implements Lock.成员变量：Sync sync
+ *        【Sync extends AbstractQueuedSynchronizer】
  * （3）
  * （4）
  * （5）
