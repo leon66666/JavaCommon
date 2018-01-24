85eb48852745841bec4089aad49aa4fac83672f4
leon66666 <1902518544@qq.com>
2018-01-24 18:49:52 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/locks/ReentrantLockDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/locks/ReentrantLockDemo.java
@@ -4,33 +4,37 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockDemo {
-	public static void main(String[] args) {
-		final Outputter1 output = new Outputter1();
-		new Thread() {
-			public void run() {
-				output.output("zhangsan");
-			};
-		}.start();
-		new Thread() {
-			public void run() {
-				output.output("lisi");
-			};
-		}.start();
-	}
+    public static void main(String[] args) {
+        final Outputter1 output = new Outputter1();
+        new Thread() {
+            public void run() {
+                output.output("zhangsan");
+            }
+
+            ;
+        }.start();
+        new Thread() {
+            public void run() {
+                output.output("lisi");
+            }
+
+            ;
+        }.start();
+    }
 }
 
 class Outputter1 {
-	private Lock lock = new ReentrantLock();// 锁对象
+    private ReentrantLock lock = new ReentrantLock();// 锁对象
 
-	public void output(String name) {
-		// TODO 线程输出方法
-		lock.lock();// 得到锁
-		try {
-			for (int i = 0; i < name.length(); i++) {
-				System.out.print(name.charAt(i));
-			}
-		} finally {
-			lock.unlock();// 释放锁
-		}
-	}
+    public void output(String name) {
+        // TODO 线程输出方法
+        lock.lock();// 得到锁
+        try {
+            for (int i = 0; i < name.length(); i++) {
+                System.out.print(name.charAt(i));
+            }
+        } finally {
+            lock.unlock();// 释放锁
+        }
+    }
 }
