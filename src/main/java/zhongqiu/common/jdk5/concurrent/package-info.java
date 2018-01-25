614fb12937dceee49087dc4341be8fee3841124e
leon66666 <1902518544@qq.com>
2018-01-25 10:40:51 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
@@ -5,7 +5,9 @@
  *        【Segment extends ReentrantLock】HashEntry<K,V>[] table;modCount;threshold;count;loadFactor
  *        【HashEntry】final hash；final key；value；HashEntry<K,V> next;
  * （2）ReentrantLock implements Lock.成员变量：Sync sync
- *        【Sync extends AbstractQueuedSynchronizer】
+ *         非公平锁：当锁处于无线程占有的状态，此时其他线程和在队列中等待的线程都可以抢占该锁。
+ *         公平锁：当锁处于无线程占有的状态，在其他线程抢占该锁的时候，都需要先进入队列中等待。
+ *        【Sync extends AbstractQueuedSynchronizer】【】
  * （3）
  * （4）
  * （5）
