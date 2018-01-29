6f38615e234e3dd65b26d7e36785fe41348a2435
leon66666 <1902518544@qq.com>
2018-01-29 18:41:15 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/locks/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/locks/package-info.java
@@ -1,8 +1,13 @@
-/**
- * 
- */
-/**
+/*
  * @author wangzhongqiu
- *
+ *可重入锁。ReentrantLock ReentrantLock implements Lock。成员变量：Sync sync;
+ *      【Sync extends AbstractQueuedSynchronizer】成员变量：long stateOffset;int state
+ *      【AbstractQueuedSynchronizer extends AbstractOwnableSynchronizer】成员变量：Thread exclusiveOwnerThread;
+ *      【NonfairSync extends Sync】非公平锁：当锁处于无线程占有的状态，此时其他线程和在队列中等待的线程都可以抢占该锁。
+ *      【FairSync extends Sync】公平锁：当锁处于无线程占有的状态，在其他线程抢占该锁的时候，都需要先进入队列中等待。
+ *      【NonfairSync】lock,
+ *       lock方法，if compareAndSetState(0, 1);unsafe.compareAndSwapInt(this, stateOffset, expect, update);
+ *       setExclusiveOwnerThread(Thread.currentThread());else acquire(1);tryAcquire(1);nonfairTryAcquire(1);
+ *       nonfairTryAcquire加锁失败，放入队列中继续尝试获得锁acquireQueued(addWaiter(Node.EXCLUSIVE), arg))
  */
 package zhongqiu.common.jdk5.concurrent.locks;
--- src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
@@ -5,16 +5,9 @@
  *      【说明】CAS原语可以用来实现无锁的数据结构。是CPU指令级的操作，只有一步原子操作
  *      【步骤】有一些状态，创建它的副本，修改它，执行CAS，如果失败，重复尝试
  *      【存在问题】如ABA问题、指令重排序等。
- * （1）ConcurrentHashMap.持有对象：Segment<K,V>[] segments;segmentMask;segmentShift
+ * （2）ConcurrentHashMap.持有对象：Segment<K,V>[] segments;segmentMask;segmentShift
  *      【Segment extends ReentrantLock】HashEntry<K,V>[] table;modCount;threshold;count;loadFactor
  *      【HashEntry】final hash；final key；value；HashEntry<K,V> next;
- * （2）可重入锁。ReentrantLock implements Lock。成员变量：Sync sync;
- *      【Sync extends AbstractQueuedSynchronizer】
- *      【NonfairSync extends Sync】非公平锁：当锁处于无线程占有的状态，此时其他线程和在队列中等待的线程都可以抢占该锁。
- *      【FairSync extends Sync】公平锁：当锁处于无线程占有的状态，在其他线程抢占该锁的时候，都需要先进入队列中等待。
- *      【FairSync】lock方法，if compareAndSetState(0, 1);unsafe.compareAndSwapInt(this, stateOffset, expect, update);
- *                           setExclusiveOwnerThread(Thread.currentThread());else acquire(1);
- * （3）
  * （4）
  * （5）
  * （6）
