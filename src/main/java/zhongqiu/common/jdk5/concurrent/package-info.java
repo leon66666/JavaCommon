440d2762c73617cb69c17f99c18c6d6f48df9435
leon66666 <1902518544@qq.com>
2018-02-02 18:31:07 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
@@ -1,6 +1,6 @@
 /*
  * @author zhongqiu
- * jdk5，并发新特性
+ * jdk5，并发新特性。JUC  http://ifeve.com/doug-lea/
  * （2）ConcurrentHashMap.持有对象：Segment<K,V>[] segments;segmentMask;segmentShift
  *      【Segment extends ReentrantLock】HashEntry<K,V>[] table;modCount;threshold;count;loadFactor
  *      【HashEntry】final hash；final key；value；HashEntry<K,V> next;
