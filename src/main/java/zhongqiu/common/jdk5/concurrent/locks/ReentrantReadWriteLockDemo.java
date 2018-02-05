e26e95610e9827f4282c30dd49232c73cd327caa
leon66666 <1902518544@qq.com>
2018-02-05 11:26:36 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/locks/ReentrantReadWriteLockDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/locks/ReentrantReadWriteLockDemo.java
@@ -4,14 +4,8 @@ import java.util.Random;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-/*
-* http://blog.csdn.net/ghsau/article/details/7461369/
-* 1. Lock的加锁和解锁都是由java代码配合native方法（调用操作系统的相关方法）实现的，而synchronize的加锁和解锁的过程是由JVM管理的
-* 2. 当一个线程使用synchronize获取锁时，若锁被其他线程占用着，那么当前只能被阻塞，直到成功获取锁。
-*    而Lock则提供超时锁和可中断等更加灵活的方式，在未能获取锁的条件下提供一种退出的机制。
-* 3. 一个锁内部可以有多个Condition实例，即有多路条件队列，而synchronize只有一路条件队列；
-*    同样Condition也提供灵活的阻塞方式，在未获得通知之前可以通过中断线程以及设置等待时限等方式退出条件队列。
-* 4. synchronize对线程的同步仅提供独占模式，而Lock即可以提供独占模式，也可以提供共享模式
+/*读写锁
+* 1.读锁，很多人同时读，但不能同时写；写锁，只能有一个人在写，且不能同时读取
 * */
 public class ReentrantReadWriteLockDemo {
     public static void main(String[] args) {
