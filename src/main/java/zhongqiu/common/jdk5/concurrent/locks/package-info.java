67d8f9ff985a0f6c1cb943db17eb1ac8727b4dac
leon66666 <1902518544@qq.com>
2018-02-07 11:57:47 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/locks/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/locks/package-info.java
@@ -30,12 +30,13 @@
  *        自旋入队列addWaiter,tail不为空，node放入队尾;tail为空，自旋for循环直到初始化并且放入队尾成功。compareAndSetHead,compareAndSetTail
  *        acquireQueued。for无限循环。自旋在阻塞队列中竞争锁
  *                   判断node节点的pre节点是否为头结点,是则node去竞争锁,获取到锁,把node置成头结点,return interrupted(初始为false)
- *                   否则判断node前节点的waitStatus是否为-1(等待锁状态)
+ *                   shouldParkAfterFailedAcquire:否则判断node前节点的waitStatus是否为-1(等待锁状态)
  *                   是则返回true。调用LockSupport.park(this)阻塞等待许可，获取许可，更新interrupted的值为Thread.interrupted(),重新下一次for循环
  *                   不是则跳过状态为1(取消等待锁)的pre节点，返回false，重新下一次for循环
  *        acquireQueued 返回true，Thread.currentThread().interrupt();
  *    【unlock方法】
  *      tryRelease(1);unparkSuccessor(h)，如果h的waitstatus为-1，修改状态为0，unpark头结点h的next
  * (2)读写锁 ReentrantReadWriteLock
+ *      【readLock().lock()】if (tryAcquireShared(arg) < 0) doAcquireShared(arg);
  */
 package zhongqiu.common.jdk5.concurrent.locks;
