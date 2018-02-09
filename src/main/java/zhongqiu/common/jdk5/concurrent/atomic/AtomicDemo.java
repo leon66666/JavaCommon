80c66c345ee41795349913f10e25ada04c3dc418
leon66666 <1902518544@qq.com>
2018-02-09 15:18:01 +0800

    first version

---
+++ src/main/java/zhongqiu/common/jdk5/concurrent/atomic/AtomicDemo.java
@@ -0,0 +1,19 @@
+package zhongqiu.common.jdk5.concurrent.atomic;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+/**
+ * @author wangzhongqiu
+ * @date 2018/2/9.
+ */
+public class AtomicDemo {
+    public static void main(String[] args) {
+        AtomicBoolean atomicBoolean = new AtomicBoolean();
+        atomicBoolean.getAndSet(true);
+//        AtomicIntegerArray atomicIntegerArray=new AtomicIntegerArray();
+//        atomicIntegerArray.getAndIncrement(0);
+
+    }
+
+}
