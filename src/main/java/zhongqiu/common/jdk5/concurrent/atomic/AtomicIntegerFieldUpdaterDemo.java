1ae9039c95961421175fe52dfc5616dcdfeabeec
leon66666 <1902518544@qq.com>
2018-02-12 18:01:25 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/atomic/AtomicIntegerFieldUpdaterDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -1,6 +1,8 @@
 package zhongqiu.common.jdk5.concurrent.atomic;
 
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /**
  * @author wangzhongqiu
@@ -8,18 +10,29 @@ import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
  */
 public class AtomicIntegerFieldUpdaterDemo {
     public static void main(String[] args) {
-        Person person = new Person("zhangsan", 11);
+        Person person = new Person("zhangsan", 11, 170);
+        person.setHobby(new Hobby("打球", "足球，篮球"));
         AtomicIntegerFieldUpdater<Person> atomicIntegerFieldUpdater = AtomicIntegerFieldUpdater.newUpdater(Person.class, "age");
         atomicIntegerFieldUpdater.addAndGet(person, 12);
+
+        AtomicLongFieldUpdater<Person> atomicLongFieldUpdater = AtomicLongFieldUpdater.newUpdater(Person.class, "height");
+        atomicLongFieldUpdater.addAndGet(person, 180);
+
+        AtomicReferenceFieldUpdater<Person, Hobby> atomicReferenceFieldUpdater = AtomicReferenceFieldUpdater.newUpdater(Person.class, Hobby.class, "hobby");
+        atomicReferenceFieldUpdater.getAndSet(person, new Hobby("打球", "排球，羽毛球"));
     }
 
     static class Person {
         private String name;
         private int age;
+        private long height;
 
-        public Person(String name, int age) {
+        private Hobby hobby;
+
+        public Person(String name, int age, long height) {
             this.name = name;
             this.age = age;
+            this.height = height;
         }
 
         public String getName() {
@@ -38,9 +51,36 @@ public class AtomicIntegerFieldUpdaterDemo {
             this.age = age;
         }
 
+        public long getHeight() {
+            return height;
+        }
+
+        public void setHeight(long height) {
+            this.height = height;
+        }
+
+        public Hobby getHobby() {
+            return hobby;
+        }
+
+        public void setHobby(Hobby hobby) {
+            this.hobby = hobby;
+        }
+
         @Override
         public String toString() {
             return "[name: " + this.name + ", age: " + this.age + "]";
+        }
+    }
+
+    static class Hobby {
+        private String hobbyName;
+
+        private String info;
+
+        public Hobby(String hobbyName, String info) {
+            this.hobbyName = hobbyName;
+            this.info = info;
         }
     }
 }
--- src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/package-info.java
@@ -8,7 +8,12 @@
  *             原子更新数组(AtomicIntegerArray,AtomicLongArray,AtomicReferenceArray)、
  *             原子更新引用(AtomicReference)、
  *             原子更新属性(AtomicIntegerFieldUpdater,AtomicLongFieldUpdater,AtomicReferenceFieldUpdater)
- *
+ *             AtomicReferenceFieldUpdater的使用有以下限制：
+ *                 字段必须是volatile类型的，在线程之间共享变量时保证立即可见
+ *                 只能是实例变量，不能是类变量，也就是说不能加static关键字
+ *                 只能是可修改变量，不能使final变量，因为final的语义就是不可修改。
+ *                 对于AtomicIntegerFieldUpdater和AtomicLongFieldUpdater只能修改int/long类型的字段，不能修改其包装类型（Integer/Long）。
+ *                 如果要修改包装类型就需要使用AtomicReferenceFieldUpdater。
  *
  * （5）locks：LockSupport，ReentrantLock，ReentrantReadWriteLock
  * （6）
