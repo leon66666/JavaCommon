a795d6a59447c657de2e9964f3e58501b5056f5b
leon66666 <1902518544@qq.com>
2018-02-27 14:20:20 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/atomic/AtomicMarkableReferenceDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/atomic/AtomicMarkableReferenceDemo.java
@@ -1,9 +1,16 @@
 package zhongqiu.common.jdk5.concurrent.atomic;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 /**
  * @author wangzhongqiu
  * @date 2018/2/27.
  */
 public class AtomicMarkableReferenceDemo {
+    public static void main(String[] args) {
+        Integer a = 1;
+        AtomicMarkableReference atomicMarkableReference = new AtomicMarkableReference(a, false);
+        atomicMarkableReference.compareAndSet(1, 2, false, true);
+    }
 
 }
