40d4d796ff5687f977470993d47e55f8b9436086
leon66666 <1902518544@qq.com>
2018-02-28 15:39:36 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/CountDownLatchDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/CountDownLatchDemo.java
@@ -1,8 +1,37 @@
 package zhongqiu.common.jdk5.concurrent;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author wangzhongqiu
  * @date 2018/2/28.
  */
 public class CountDownLatchDemo {
+    public static void main(String[] args) {
+        int count = 2;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        Cal cal = new Cal(countDownLatch);
+        new Thread(cal).start();
+        new Thread(cal).start();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("all done");
+    }
+
+    static class Cal implements Runnable {
+        private CountDownLatch countDownLatch;
+
+        public Cal(CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("完成计算");
+            countDownLatch.countDown();
+        }
+    }
 }
