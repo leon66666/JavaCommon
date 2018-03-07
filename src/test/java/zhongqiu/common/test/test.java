83e9efec6517eb3fe8159b13551bbf7df8fc1bc1
leon66666 <1902518544@qq.com>
2018-03-07 22:53:38 +0800

    first version

--- src/test/java/zhongqiu/common/test/test.java
+++ src/test/java/zhongqiu/common/test/test.java
@@ -1,24 +1,19 @@
 package zhongqiu.common.test;
-/*
-*
-* */
 
-import zhongqiu.common.base.thread.SynchronizedDemo;
-
-import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class test {
     public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue=new ArrayBlockingQueue<Integer>(4);
         try {
-            System.out.println("1");
-            return;
-        } finally {
-            System.out.println("2");
+            arrayBlockingQueue.put(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-//        System.out.println("3");
-//        SynchronizedDemo
+        try {
+            arrayBlockingQueue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
