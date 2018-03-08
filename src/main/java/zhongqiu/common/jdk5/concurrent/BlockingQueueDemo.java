6526607587c57f1d3e0cd398f86b55d766132d68
leon66666 <1902518544@qq.com>
2018-03-08 16:31:43 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/BlockingQueueDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/BlockingQueueDemo.java
@@ -13,7 +13,7 @@ import java.util.concurrent.*;
 * 常用方法：放入元素，add，offer，put     获取元素，peek，poll，take，drainTo
 * ArrayBlockingQueue 是一个有界的阻塞队列，其内部实现是将对象放到一个循环数组里。它不能够存储无限多数量的元素。
 *                    它有一个同一时间能够存储元素数量的上限。你可以在对其初始化的时候设定这个上限，但之后就无法对这个上限进行修改了
-*    方法源码分析：同时有三个线程调用put方法，线程1执行lock.lockInterruptibly()加锁，线程2和线程3放入到lock的aqs队列中等待锁，
+*     方法源码分析：同时有三个线程调用put方法，线程1执行lock.lockInterruptibly()加锁，线程2和线程3放入到lock的aqs队列中等待锁，
 *                 假设阻塞队列中已达到最大值，线程1执行notFull.await();执行addConditionWaiter()把线程1放到nullFull条件队列中，
 *                 执行fullyRelease唤醒aqs队列中的下一个节点，线程1阻塞；以此类推，线程2和线程3也被放入到了nullFull条件队列中。
 *                 全部阻塞在了notFull.await()方法内部。线程4调用take方法，执行lock.lockInterruptibly()加锁，
@@ -24,23 +24,28 @@ import java.util.concurrent.*;
 *                     使用了put和take两个锁进行锁分离，效率比ArrayBlockingQueue高
 * SynchronousQueue 阻塞队列，基于Spin和CAS方法。其中每个插入操作必须等待另一个线程的对应移除操作 ，反之亦然。
 *                  同步队列没有任何内部容量，甚至连一个队列的容量都没有。
-*    TransferQueue 先进先出队列，公平模式。核心方法,transfer
+*     TransferQueue 先进先出队列，公平模式。核心方法,transfer
 *
 * PriorityBlockingQueue 基于数组实现的线程安全的无界优先级队列，你无法向这个队列中插入 null 值。
 *                       所有插入到 PriorityBlockingQueue 的元素必须实现 java.lang.Comparable 接口
 *                       以数组的形式实现的最大或最小堆.
-*   添加方法：add,offer,put，最终调用的都是offer方法，因为是无界队列，入队列不会阻塞。
-*   offer方法： lock.lock()加锁，tryGrow(array, cap)增加容量，siftUpUsingComparator(n, e, array, cmp)自下而上调整，
+*     添加方法：add,offer,put，最终调用的都是offer方法，因为是无界队列，入队列不会阻塞。
+*     offer方法： lock.lock()加锁，tryGrow(array, cap)增加容量，siftUpUsingComparator(n, e, array, cmp)自下而上调整，
 *               notEmpty.signal()唤醒take线程。lock.unlock()解锁
-*   获取方法：如果队列中没有元素，获取失败，线程park，进入waiting状态。
-*   take方法： lock.lockInterruptibly()加锁，while ( (result = dequeue()) == null) notEmpty.await()。最后lock.unlock()解锁
+*     获取方法：如果队列中没有元素，获取失败，线程park，进入waiting状态。
+*     take方法： lock.lockInterruptibly()加锁，while ( (result = dequeue()) == null) notEmpty.await()。最后lock.unlock()解锁
 * DelayQueue  无界的优先级的阻塞队列，其中的对象只能在其到期时才能从队列中取走。
+*             DelayQueue<E extends Delayed>,需实现方法long getDelay(TimeUnit unit);public int compareTo(T o);
+*     添加方法：add,offer,put，最终调用的都是offer方法，因为是无界队列，入队列不会阻塞。
+*
+*
 * */
 public class BlockingQueueDemo {
     private ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(100);
     private LinkedBlockingQueue<Integer> linkedBlockingQueue = new LinkedBlockingQueue<>();
     private SynchronousQueue<Integer> synchronousQueue = new SynchronousQueue<>();
     private PriorityBlockingQueue<Integer> priorityBlockingQueue = new PriorityBlockingQueue<>();
+    private DelayQueue<Student> delayQueue = new DelayQueue<>();
 
     public static void main(String[] args) {
         ArrayBlockingQueueTest();
@@ -208,7 +213,7 @@ public class BlockingQueueDemo {
             try {
                 System.out.println(" test start");
                 while (counter > 0) {
-                    Student student = students.poll();
+                    Student student = students.take();
                     if (student.workTime < 120) {
                         student.run();
                         if (counter > 0) {
