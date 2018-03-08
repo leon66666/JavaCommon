590272c7136a054e964e2ff502de7ea249e160f7
leon66666 <1902518544@qq.com>
2018-03-08 11:54:46 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/BlockingQueueDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/BlockingQueueDemo.java
@@ -24,8 +24,8 @@ import java.util.concurrent.*;
 *                     使用了put和take两个锁进行锁分离，效率比ArrayBlockingQueue高
 * SynchronousQueue 阻塞队列，基于Spin和CAS方法。其中每个插入操作必须等待另一个线程的对应移除操作 ，反之亦然。
 *                  同步队列没有任何内部容量，甚至连一个队列的容量都没有。
-*                  非常适合于传递性设计，在这种设计中，在一个线程中运行的对象要将某些信息、事件或任务传递给在另一个线程中运行的对象，
-*                  它就必须与该对象同步。
+*    TransferQueue 先进先出队列，公平模式。核心方法,transfer
+*
 * PriorityBlockingQueue 基于数组实现的线程安全的无界优先级队列，你无法向这个队列中插入 null 值。
 *                       所有插入到 PriorityBlockingQueue 的元素必须实现 java.lang.Comparable 接口
 *                       以数组的形式实现的最大或最小堆.
