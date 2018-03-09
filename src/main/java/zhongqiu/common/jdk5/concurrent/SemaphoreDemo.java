0c6615ad433e2e0b7a345b97c9ee282c06063003
leon66666 <1902518544@qq.com>
2018-03-09 11:31:36 +0800

    first version

--- src/main/java/zhongqiu/common/jdk5/concurrent/SemaphoreDemo.java
+++ src/main/java/zhongqiu/common/jdk5/concurrent/SemaphoreDemo.java
@@ -4,6 +4,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+/*
+* 实现原理，和countdownlatch类似，内部类sync实现了AbstractQueuedSynchronizer
+* 使用场景：Semaphore经常用于限制获取某种资源的线程数量。
+*          比如说操场上有5个跑道，一个跑道一次只能有一个学生在上面跑步，一旦所有跑道在使用，
+*          那么后面的学生就需要等待，直到有一个学生不跑了
+* */
 public class SemaphoreDemo {
     public static void main(String[] args) {
         // 线程池
