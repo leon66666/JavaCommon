bc12e09f4cbb1e91dcc2d946d328a8576610cbb0
leon66666 <1902518544@qq.com>
2018-03-19 14:26:35 +0800

    first version

--- src/main/java/zhongqiu/common/base/collections/CollectionsDemo.java
+++ src/main/java/zhongqiu/common/base/collections/CollectionsDemo.java
@@ -1,8 +1,6 @@
 package zhongqiu.common.base.collections;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author wangzhongqiu
@@ -10,11 +8,18 @@ import java.util.List;
  */
 public class CollectionsDemo {
     private static ArrayList<String> lists = new ArrayList<>();
+    private static HashSet<String> set = new HashSet<>();
+    private static HashMap<String, String> map = new HashMap<>();
 
     public static void main(String[] args) {
         lists.add("1");
         lists.add("2");
         lists.add("3");
+        set.add("1");
+        map.put("1", "1");
+
         List<String> synlist = Collections.synchronizedList(lists);
+        Set<String> synset = Collections.synchronizedSet(set);
+        Map<String, String> synmap = Collections.synchronizedMap(map);
     }
 }
